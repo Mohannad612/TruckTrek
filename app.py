f5b315f988a28d4a86d6ca1b98dbb55416fe226d
Mohammed-hub991 <mohammedsaad7997@gmail.com>
2024-09-30 12:20:26 +0800

    Updated Signup and Login
    This is the updated signup and login code, with the new UI as well as the new feature "forget Password" being there..

--- app.py
+++ app.py
@@ -1,41 +1,37 @@
-from flask import Flask, render_template, url_for, redirect, flash
+from flask import Flask, render_template, url_for, redirect, flash, request
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, HiddenField
+from wtforms.validators import InputRequired, Length, ValidationError, Email
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail, Message
 
-# Initialize the Flask application
 app = Flask(__name__)
 
-# Set up the application configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # This sets up the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SecureHome'
 
-# Initialize the database
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'crazylogic749@gmail.com'
-app.config['MAIL_PASSWORD'] = 'lnws rcaa esrf djrv'  # Replace with the app password you generated
+app.config['MAIL_PASSWORD'] = 'lnws rcaa esrf djrv'
 
 mail = Mail(app)
 
-# Define the User model
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), nullable=False, unique=True)  # Ensure this is an email if used for sending emails
+    username = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    
+
     def get_token(self, expires_sec=300):
-        serial = Serializer(app.config['SECRET_KEY'], expires_in=expires_sec)
+        serial = Serializer(app.config['SECRET_KEY'], expires_sec)
         return serial.dumps({'user_id': self.id}).decode('utf-8')
-    
+
     @staticmethod
     def verify_token(token):
         serial = Serializer(app.config['SECRET_KEY'])
@@ -45,59 +41,61 @@ class User(db.Model, UserMixin):
             return None
         return User.query.get(user_id)
 
-# Initialize the database and create tables
 with app.app_context():
     db.create_all()
 
-# Set up Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth'
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Define the Registration form
-class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=120)], render_kw={"placeholder": "Email"})
+class AuthForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Email(), Length(min=4, max=120)], render_kw={"placeholder": "Email", "type": "email"})
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-    submit = SubmitField('Register')
+    submit = SubmitField('Submit')
+    mode = HiddenField('mode', default='login')
 
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(username=username.data).first()
-        if existing_user_username:
-            raise ValidationError('That username already exists. Please choose a different one.')
+        if self.mode.data == 'register':
+            existing_user_username = User.query.filter_by(username=username.data).first()
+            if existing_user_username:
+                raise ValidationError('That username already exists. Please choose a different one.')
 
-# Define the Login form
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=120)], render_kw={"placeholder": "Email"})
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-    submit = SubmitField('Login')
-    
 class ResetRequestForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=120)], render_kw={"placeholder": "Email"})
-    submit = SubmitField(label='Reset Password')
+    username = StringField(validators=[InputRequired(), Email(), Length(min=4, max=120)], render_kw={"placeholder": "Email", "type": "email"})
+    submit = SubmitField('Submit')
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "New Password"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     confirm_password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Confirm Password"})
-    submit = SubmitField('Change Password')
+    submit = SubmitField('Submit')
 
-# Define the routes
 @app.route('/')
-def home():
-    return render_template('home.html')
+def start():
+    return render_template('start.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    form = AuthForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user)
-            return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
+        if form.mode.data == 'login':
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Login Unsuccessful. Please check username and password', 'danger')
+        elif form.mode.data == 'register':
+            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            new_user = User(username=form.username.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Your account has been created! You are now able to log in', 'success')
+            return redirect(url_for('auth'))
+    return render_template('auth.html', form=form)
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
@@ -108,18 +106,7 @@ def dashboard():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+    return redirect(url_for('auth'))
 
 def send_mail(user):
     token = user.get_token()
@@ -139,8 +126,10 @@ def reset_request():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
             send_mail(user)
-        flash('Reset request sent. Check your email.', 'success')
-        return redirect(url_for('login'))
+            flash('Reset request sent. Check your email.', 'success')
+            return redirect(url_for('auth'))
+        else:
+            flash('No account found with that email.', 'danger')
     return render_template('reset_request.html', title='Reset Request', form=form)
 
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
@@ -149,7 +138,7 @@ def reset_token(token):
     if user is None:
         flash('That is an invalid or expired token. Please try again.', 'warning')
         return redirect(url_for('reset_request'))
-    
+
     form = ResetPasswordForm()
     if form.validate_on_submit():
         if form.password.data != form.confirm_password.data:
@@ -159,8 +148,8 @@ def reset_token(token):
             user.password = hashed_password
             db.session.commit()
             flash('Your password has been updated! You are now able to log in.', 'success')
-            return redirect(url_for('login'))
-    
+            return redirect(url_for('auth'))
+
     return render_template('change_password.html', title="Change Password", form=form)
 
 if __name__ == "__main__":
